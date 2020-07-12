9065f17897b276980e5ea5ed4b347bd278cb12c3
systemsauditor@shkmbk.ae <67480042+shkmabk@users.noreply.github.com>
2020-07-12 11:20:19 +0400

    Revalution Report Fix
    Revalution Report Fix

--- custom/mis_investment/wizard/share_revaluation_report_wizard.py
+++ custom/mis_investment/wizard/share_revaluation_report_wizard.py
@@ -11,11 +11,13 @@ class ShareRevaluationReport(models.TransientModel):
     date_from = fields.Date(default=fields.Date.to_string(date.today()), required="1")
     date_to = fields.Date(default=fields.Date.to_string(date.today()), required="1")
 
+
     def button_export_pdf(self):
         data = {}
-        #raise  UserError(self.date_to+ timedelta(days=1))
+
         data['date_from'] = self.date_from
         data['date_to'] = self.date_to
+        data['header_date'] = self.date_to
         report = self.env.ref(
             'mis_investment.action_share_revalution_report')
         return report.report_action(self, data=data)
