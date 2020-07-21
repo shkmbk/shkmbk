75e92cc727ad08ba1bbf703633c97bb79b7e08a7
systemsauditor@shkmbk.ae <67480042+shkmabk@users.noreply.github.com>
2020-07-21 12:15:11 +0400

    Asset code fix
    Asset code fix

--- custom/mis_auh_asset_extend/__manifest__.py
+++ custom/mis_auh_asset_extend/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '1.1.1',
     'category': 'Accounting',
     'summary': """ """,
-    'description': """
+    'description': """ Account Asset Extend
                     """,
     'author': "Sananaz Mansuri",
     'website': 'Mindifnosys.com',
