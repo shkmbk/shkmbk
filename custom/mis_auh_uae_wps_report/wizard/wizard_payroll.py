2b15c21a7454c0adadc32ebe9919ae694e2be365
systemsauditor@shkmbk.ae <67480042+shkmabk@users.noreply.github.com>
2020-08-12 13:20:37 +0400

    Payroll Report
    Payroll Report

--- custom/mis_auh_uae_wps_report/wizard/wizard_payroll.py
+++ custom/mis_auh_uae_wps_report/wizard/wizard_payroll.py
@@ -129,7 +129,6 @@ class WizardPayroll(models.TransientModel):
         wbf['header'] = workbook.add_format(
             {'bold': 1, 'align': 'center'})
         wbf['content_float'] = workbook.add_format({'align': 'right', 'num_format': '#,##0.00'})
-
         wbf['content_border'] = workbook.add_format()
         wbf['content_border'].set_top()
         wbf['content_border'].set_bottom()
@@ -141,108 +140,150 @@ class WizardPayroll(models.TransientModel):
         wbf['content_float_border'].set_left()
         wbf['content_float_border'].set_right()
 
+        wbf['content_border_bg_total'] = workbook.add_format({'align': 'right', 'bold': 1, 'bg_color': '#E1E1E1'})
+        wbf['content_border_bg_total'].set_top()
+        wbf['content_border_bg_total'].set_bottom()
+        wbf['content_border_bg_total'].set_left()
+        wbf['content_border_bg_total'].set_right()
+
+        wbf['content_border_bg'] = workbook.add_format({'bold': 1, 'bg_color': '#E1E1E1'})
+        wbf['content_border_bg'].set_top()
+        wbf['content_border_bg'].set_bottom()
+        wbf['content_border_bg'].set_left()
+        wbf['content_border_bg'].set_right()
+        wbf['content_float_border_bg'] = workbook.add_format({'align': 'right', 'num_format': '#,##0.00', 'bold': 1, 'bg_color': '#E1E1E1'})
+        wbf['content_float_border_bg'].set_top()
+        wbf['content_float_border_bg'].set_bottom()
+        wbf['content_float_border_bg'].set_left()
+        wbf['content_float_border_bg'].set_right()
+
+
 
         worksheet = workbook.add_worksheet(report_name)
 
         col=0
-        column_width=15
+        column_width=10
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Sl.  No.', wbf['content_border'])
+        worksheet.write(0, col, 'Sl.  No.', wbf['content_border_bg'])
         col = 1
-        column_width = 15
+        column_width = 30
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Employee Name', wbf['content_border'])
+        worksheet.write(0, col, 'Employee Name', wbf['content_border_bg'])
         col = 2
-        column_width = 15
+        column_width = 30
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Designation', wbf['content_border'])
+        worksheet.write(0, col, 'Designation', wbf['content_border_bg'])
         col = 3
         column_width = 30
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Division', wbf['content_border'])
+        worksheet.write(0, col, 'Division', wbf['content_border_bg'])
         col = 4
         column_width = 30
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Department', wbf['content_border'])
+        worksheet.write(0, col, 'Department', wbf['content_border_bg'])
         col = 5
         column_width = 20
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Payment Mode', wbf['content_border'])
+        worksheet.write(0, col, 'Payment Mode', wbf['content_border_bg'])
         col = 6
         column_width = 30
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Contract Salary (Basic+Allowance)', wbf['content_border'])
+        worksheet.write(0, col, 'Contract Salary (Basic+Allowance)', wbf['content_border_bg'])
         col = 7
-        column_width = 15
+        column_width = 10
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Days', wbf['content_border'])
+        worksheet.write(0, col, 'Days', wbf['content_border_bg'])
         col = 8
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Basic Salary', wbf['content_border'])
+        worksheet.write(0, col, 'Basic Salary', wbf['content_border_bg'])
         col = 9
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Allowance', wbf['content_border'])
-        col = 9
-        column_width = 15
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Fixed OT', wbf['content_border'])
-        col = 9
-        column_width = 15
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'L.Salary', wbf['content_border'])
+        worksheet.write(0, col, 'Allowance', wbf['content_border_bg'])
         col = 10
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Ticket Allowance', wbf['content_border'])
+        worksheet.write(0, col, 'Fixed OT', wbf['content_border_bg'])
         col = 11
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'EOSB', wbf['content_border'])
+        worksheet.write(0, col, 'L.Salary', wbf['content_border_bg'])
         col = 12
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Gross Amount', wbf['content_border'])
+        worksheet.write(0, col, 'Ticket Allowance', wbf['content_border_bg'])
         col = 13
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Fine & Penalty', wbf['content_border'])
+        worksheet.write(0, col, 'EOSB', wbf['content_border_bg'])
         col = 14
         column_width = 15
         worksheet.set_column(col, col, column_width)
-        worksheet.write(0, col, 'Net Salary', wbf['content_border'])
+        worksheet.write(0, col, 'Gross Amount', wbf['content_border_bg'])
+        col = 15
+        column_width = 15
+        worksheet.set_column(col, col, column_width)
+        worksheet.write(0, col, 'Fine & Penalty', wbf['content_border_bg'])
+        col = 16
+        column_width = 15
+        worksheet.set_column(col, col, column_width)
+        worksheet.write(0, col, 'Salary Advance', wbf['content_border_bg'])
+
+        col = 17
+        column_width = 15
+        worksheet.set_column(col, col, column_width)
+        worksheet.write(0, col, 'Net Salary', wbf['content_border_bg'])
 
         count = 0
         sum = 0
+        sum_basic_allow=0.0
+        sum_basic=0.0
+        sum_allowance=0.0
+        sum_fixedot=0.0
+        sum_lsalary =0.0
+        sum_ticketallow=0.0
+        sum_eosb=0.0
+        sum_gross_amount=0.0
+        sum_fine_penalty=0.0
+        sum_advance=0.0
+        sum_net=0.0
         for rec in slips:
             count += 1
             col=0
-
+            #SEQ
             worksheet.write(count, col, count,  wbf['content_border'])
             col += 1
-
+            # Name
             worksheet.write(count, col, rec.employee_id.name, wbf['content_border'])
+            # JOB
             col += 1
             worksheet.write(count, col, rec.employee_id.job_id.name, wbf['content_border'])
+            # DIVISION
             col += 1
             worksheet.write(count, col, rec.employee_id.department_id.name, wbf['content_border'])
+            # DEPARTMENT
             col += 1
-            worksheet.write(count, col, rec.employee_id.name, wbf['content_border'])
+            worksheet.write(count, col, rec.employee_id.department_id.name, wbf['content_border'])
+            # PAYMENT METHOD
+            col+=1
+            worksheet.write(count, col, rec.employee_id.payment_method.name,  wbf['content_border'])
+            # CONTRACT SALARY (BASIC + ALLOWANCE)
             col+=1
 
-            worksheet.write(count, col, rec.employee_id.payment_method.name,  wbf['content_border'])
+            basic_allow=rec.employee_id.contract_id.wage+rec.employee_id.contract_id.x_house_rent+rec.employee_id.contract_id.x_transpot+rec.employee_id.contract_id.x_other_allowance+rec.employee_id.contract_id.x_other_allowance+rec.employee_id.contract_id.x_fixed_ot
+            worksheet.write(count, col, basic_allow,  wbf['content_float_border'])
+            # DAYS
             col+=1
-            worksheet.write(count, col, "contract amt",  wbf['content_border'])
-            col+=1
-            worksheet.write(count, col, "Days",  wbf['content_border'])
+            worksheet.write(count, col, ((self.end_date-self.start_date).days+1),  wbf['content_border'])
+            # BASIC AFTER UNPAID
             col += 1
             amountbasic = 0.0
             slipbasic = self.env['hr.payslip.line'].search([('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'BASIC')])
             if slipbasic:
                 amountbasic=slipbasic.total
             worksheet.write(count, col, amountbasic, wbf['content_float_border'])
-
+            # ALLOWANCE AFTER UNPAID
             col += 1
             amountallow = 0.0
             sliphra = self.env['hr.payslip.line'].search(
@@ -250,7 +291,7 @@ class WizardPayroll(models.TransientModel):
             if sliphra:
                 amountallow = sliphra.total
             worksheet.write(count, col, amountallow, wbf['content_float_border'])
-
+            # FIX OT
             col += 1
             amountfixot = 0.0
             slipfixot = self.env['hr.payslip.line'].search(
@@ -258,7 +299,7 @@ class WizardPayroll(models.TransientModel):
             if slipfixot:
                 amountfixot = slipfixot.total
             worksheet.write(count, col, amountfixot, wbf['content_float_border'])
-
+            # LEAVE SALARY
             col += 1
             amountlsal = 0.0
             sliplsal = self.env['hr.payslip.line'].search(
@@ -266,15 +307,15 @@ class WizardPayroll(models.TransientModel):
             if sliplsal:
                 amountlsal = sliplsal.total
             worksheet.write(count, col, amountlsal, wbf['content_float_border'])
-
+            # TICKET ALLOWANCE
             col += 1
             amounttall = 0.0
             sliptall = self.env['hr.payslip.line'].search(
-                [('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'TALL')])
+                [('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'AT')])
             if sliptall:
                 amounttall = sliptall.total
             worksheet.write(count, col, amounttall, wbf['content_float_border'])
-
+            # EOSB
             col += 1
             amounteosb= 0.0
             slipeosb = self.env['hr.payslip.line'].search(
@@ -282,7 +323,7 @@ class WizardPayroll(models.TransientModel):
             if slipeosb:
                 amounteosb = slipeosb.total
             worksheet.write(count, col, amounteosb, wbf['content_float_border'])
-
+            # GROSS
             col += 1
             amountgross= 0.0
             slipgross = self.env['hr.payslip.line'].search(
@@ -290,34 +331,68 @@ class WizardPayroll(models.TransientModel):
             if slipgross:
                 amountgross = slipgross.total
             worksheet.write(count, col, amountgross, wbf['content_float_border'])
-
+            # FINE
             col += 1
             amountfine= 0.0
             slipfine = self.env['hr.payslip.line'].search(
-                [('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'FINE')])
+                [('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'PD')])
             if slipfine:
                 amountfine = slipfine.total
             worksheet.write(count, col, amountfine, wbf['content_float_border'])
-
+            # ADVANCE
             col += 1
             amountadv= 0.0
             slipadv = self.env['hr.payslip.line'].search(
-                [('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'ADV')])
+                [('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'SA')])
             if slipadv:
                 amountadv = slipadv.total
             worksheet.write(count, col, amountadv, wbf['content_float_border'])
+            # NET
             col += 1
             amountnet = 0.0
             slipnet = self.env['hr.payslip.line'].search([('slip_id', '=', rec.id), ('employee_id', '=', rec.employee_id.id), ('code', '=', 'NET')])
             if slipnet:
                 amountnet=slipnet.total
             worksheet.write(count, col, amountnet, wbf['content_float_border'])
+            #ADDING to sum
+            sum_basic_allow += basic_allow
+            sum_basic += amountbasic
+            sum_allowance += amountallow
+            sum_fixedot += amountfixot
+            sum_lsalary += amountlsal
+            sum_ticketallow += amounttall
+            sum_eosb += amounteosb
+            sum_gross_amount += amountgross
+            sum_fine_penalty += amountfine
+            sum_advance += amountadv
+            sum_net += amountnet
 
         count+=2
-#        worksheet.merge_range('A%s:D%s'%(count,count), 'Total', wbf['content_border'])
-#        worksheet.write(count-1, 4, sum, wbf['content_float_border'])
-
-
+        # SUMMARY
+        worksheet.merge_range('A%s:F%s'%(count,count), 'Total', wbf['content_border_bg_total'])
+        worksheet.write(count-1, 6, sum_basic_allow, wbf['content_float_border_bg'])
+        worksheet.write(count - 1, 7, "", wbf['content_border_bg'])
+        col=7
+        col+=1
+        worksheet.write(count - 1, col, sum_basic, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_allowance, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_fixedot, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_lsalary, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_ticketallow, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_eosb, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_gross_amount, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_fine_penalty, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_advance, wbf['content_float_border_bg'])
+        col += 1
+        worksheet.write(count - 1, col, sum_net, wbf['content_float_border_bg'])
 
         workbook.close()
         out=base64.encodestring(fp.getvalue())
@@ -330,7 +405,3 @@ class WizardPayroll(models.TransientModel):
             'target': 'new',
             'url': 'web/content/?model='+self._name+'&id='+str(self.id)+'&field=datas&download=true&filename='+filename,
         }
-
-
-
-
