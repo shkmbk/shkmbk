40cdf61843bfb2bc031e53eae809e3e49a511ddc
systemsauditor@shkmbk.ae <67480042+shkmabk@users.noreply.github.com>
2021-01-31 09:06:05 +0400

    Date Filter Added into Accounts Dashboard
    Date Filter Added into Accounts Dashboard

--- custom/mis_auh_dashboard/static/src/js/account_dashboard.js
+++ custom/mis_auh_dashboard/static/src/js/account_dashboard.js
@@ -15,124 +15,123 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
 
         events: {
             'click .invoice_dashboard': 'onclick_dashboard',
-            'click #prog_bar': 'onclick_prog_bar',
-            'click #onclick_banks_balance': 'onclick_bank_balance',
-            'click #income_this_month': 'onclick_income_this_month',
-            'click #income_this_year': 'onclick_income_this_year',
-            'click #total_aged_payable': 'onclick_total_aged_payable',
-            'click #in_ex_bar_chart': 'onclick_in_ex_bar_chart',
-            'click #aged_recevable_pie_chart': 'onclick_aged_recevable_pie_chart',
-            'click #bank_balance_hide': 'onclick_bank_balance_hide',
-            'click #cash_balance_hide': 'onclick_cash_balance_hide',
-            'click #in_ex_hide': 'onclick_in_ex_hide',
-            'click #aged_payable_hide': 'onclick_aged_payable_hide',
             'change #income_expense_values': function(e) {
                             e.stopPropagation();
                             var $target = $(e.target);
                             var value = $target.val();
                             this.$('.income_expense_values').empty();
-                            this.onclick_income_this_year(this.$('#income_expense_values').val());
-                                                        },
-            'change #toggle-two': 'onclick_toggle_two',
+                            this.onclick_income_this_year(this.$('#income_expense_values').val());                                                        },
+            'change #month_id': 'onclick_month',
 
         },
-        onclick_toggle_two: function (ev) {
+        onclick_month: function (ev) {
+            ev.stopPropagation();
+            var $target = $(ev.target);
+            var value = $target.val();
+            var currency = 'AED'
+            var sel_id =document.getElementById("income_expense_values").value;
 
-        },
-
-        onclick_income_last_year: function (ev) {
-            ev.preventDefault();
-            var selected = $('.btn.btn-tool.income');
-            var data = $(selected[0]).data();
-            var posted = false;
-            if ($('#toggle-two')[0].checked == true) {
-                posted = "posted"
+            function format_amount(amount){
+                 if (typeof(amount) != 'number'){
+                    amount = parseFloat(0.00);
+                 }
+                 var formatted_value = (parseInt(amount)).toLocaleString("en-US", {minimumFractionDigits: 2})
+                 return formatted_value += ' ' + "AED";
             }
-            rpc.query({
-                model: 'account.move',
-                method: 'get_income_last_year',
-                args: [posted],
-            })
-                .then(function (result) {
+           rpc.query({
+                model: "account.move",
+                method: "get_share_pl_summary",
+                args: [value],
+            }).then(function (result) {
+                    var due_count = 0;
+                    var amount;
+                    var total_amount = 0.00;
+                    $('#share_pl_list').empty();
+                    $('#total_share_pl').empty();
 
-                    $('#net_profit_this_months').hide();
-                    $('#net_profit_last_month').hide();
-                    $('#net_profit_last_year').show();
-                    $('#net_profit_this_year').hide();
-
-                    var ctx = document.getElementById("canvas").getContext('2d');
-
-                    // Define the data
-                    var income = result.income; // Add data values to array
-                    var expense = result.expense;
-                    var profit = result.profit;
-
-                    var labels = result.month; // Add labels to array
-                    // End Defining data
-
-                    // End Defining data
-                    if (window.myCharts != undefined)
-                        window.myCharts.destroy();
-                    window.myCharts = new Chart(ctx, {
-                        //var myChart = new Chart(ctx, {
-                        type: 'bar',
-                        data: {
-                            labels: labels,
-                            datasets: [{
-                                label: 'Income', // Name the series
-                                data: income, // Specify the data values array
-                                backgroundColor: '#66aecf',
-                                borderColor: '#66aecf',
-
-                                borderWidth: 1, // Specify bar border width
-                                type: 'bar', // Set this data to a line chart
-                                fill: false
-                            },
-                                {
-                                    label: 'Expense', // Name the series
-                                    data: expense, // Specify the data values array
-                                    backgroundColor: '#ffa500',
-                                    borderColor: '#ffa500',
-
-                                    borderWidth: 1, // Specify bar border width
-                                    type: 'bar', // Set this data to a line chart
-                                    fill: false
-                                },
-                                {
-                                    label: 'Profit/Loss', // Name the series
-                                    data: profit, // Specify the data values array
-                                    backgroundColor: '#0bd465',
-                                    borderColor: '#0bd465',
-
-                                    borderWidth: 1, // Specify bar border width
-                                    type: 'line', // Set this data to a line chart
-                                    fill: false
-                                }
-                            ]
-                        },
-                        options: {
-                            responsive: true, // Instruct chart js to respond nicely.
-                            maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height
-                        }
+                    _.forEach(result, function (x) {
+                        $('#share_pl_list').show();
+                        due_count++;
+                        amount = format_amount(x.amount);
+                        total_amount += x.amount
+                        $('#share_pl_list').append('<li><div>' + x.particulars + '</div>' + '<div>' + amount + '</div>' + '</li>');
                     });
-
+                    var f_total_amount = format_amount(total_amount);
+                    if (total_amount>0.00){
+                        $('#total_share_pl').append('<span style= "color:#008000;">' + f_total_amount + '</span>')
+                    } else if(total_amount<0.00){
+                        $('#total_share_pl').append('<span style= "color:#FF0000;">' + f_total_amount + '</span>')
+                    } else {
+                         $('#total_share_pl').append('<span style= "color:#808080;">' + f_total_amount + '</span>')
+                    }
                 })
-        },
+                rpc.query({
+                    model: "account.move",
+                    method: "get_salary_list",
+                    args: [value],
+                }).then(function (result) {
+                        var due_count = 0;
+                        var amount;
+                        var total_amount = 0.00;
+                        $('#salary_list').empty();
+                        $('#totalsalary').empty();
+                        _.forEach(result, function (x) {
+                            $('#salary_list').show();
+                            due_count++;
+                            if (x.amount != 0.00) {
+                              amount = format_amount(x.amount);
+                              total_amount += x.amount
+                              $('#salary_list').append('<li><div>' + x.particulars + '</div>' + '<div>' + amount + '</div>' + '</li>');
+                            }
+                        });
+                        total_amount = format_amount(total_amount);
+                        $('#totalsalary').append('<span>' + total_amount + '</span>')
+                    })
+                    rpc.query({
+                        model: "account.move",
+                        method: "get_accounts_profit",
+                        args: [value],
+                    })
+                        .then(function (result) {
+                            var net_profit_this_year = result[0].profit;
+                            var net_profit_this_months = result[1].profit;
+                            var incomes_this_year = result[0].income;
+                            var income_this_month = result[1].income;
+                            var expenses_this_year = result[0].expense;
+                            var expenses_this_month = result[1].expense;
+                            var count_this_year = result[0].count;
+                            var count_this_month = result[1].count;
 
-        onclick_income_this_year: function (f) {
-            //ev.preventDefault();
-            var selected = $('.btn.btn-tool.income');
-            var data = $(selected[0]).data();
-            var posted = false;
-            if ($('#toggle-two')[0].checked == true) {
-                posted = "posted"
-            }
+                            net_profit_this_year = format_amount(net_profit_this_year);
+                            net_profit_this_months = format_amount(net_profit_this_months);
+                            incomes_this_year = format_amount(incomes_this_year);
+                            income_this_month = format_amount(income_this_month);
+                            expenses_this_year = format_amount(expenses_this_year);
+                            expenses_this_month = format_amount(expenses_this_month);
 
+                            $('#net_profit_current_year').empty();
+                            $('#net_profit_current_months').empty();
+                            $('#total_incomes_this_year').empty();
+                            $('#total_incomes_').empty();
+                            $('#total_expense_this_year').empty();
+                            $('#total_expenses_').empty();
+                            $('#count_this_year').empty();
+                            $('#count_this_month').empty();
+
+                            $('#net_profit_current_year').append('<span>' + net_profit_this_year + '</span> <div class="title">This Year</div>')
+                            $('#net_profit_current_months').append('<span>' + net_profit_this_months + '</span> <div class="title">This Month</div>')
+                            $('#total_incomes_this_year').append('<span>' + incomes_this_year + '</span><div class="title">This Year</div>')
+                            $('#total_incomes_').append('<span>' + income_this_month + '</span><div class="title">This month</div>')
+                            $('#total_expense_this_year').append('<span >' + expenses_this_year + '</span><div class="title">This Year</div>')
+                            $('#total_expenses_').append('<span>' + expenses_this_month + '</span><div class="title">This month</div>')
+                            $('#count_this_year').append('<span>' + count_this_year + '</span><div class="title">This Year</div>')
+                            $('#count_this_month').append('<span>' + count_this_month + '</span><div class="title">This month</div>')
+                        })
 
             rpc.query({
                 model: 'account.move',
                 method: 'get_income_expense',
-                args: [f],
+                args: [sel_id,value],
 
             })
                 .then(function (result) {
@@ -201,22 +200,26 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
                 })
         },
 
-        onclick_income_this_month: function (ev) {
-            ev.preventDefault();
+        onclick_income_this_year: function (f) {
+            //ev.preventDefault();
             var selected = $('.btn.btn-tool.income');
             var data = $(selected[0]).data();
-            var posted = false;
-            if ($('#toggle-two')[0].checked == true) {
-                posted = "posted"
-            }
+            var posted = "posted";
+            var month_value=document.getElementById("month_id").value;
+
             rpc.query({
                 model: 'account.move',
-                method: 'get_income_this_month',
-                args: "posted",
+                method: 'get_income_expense',
+                args: [f,month_value],
 
             })
                 .then(function (result) {
 
+
+                    $('#net_profit_this_months').hide();
+                    $('#net_profit_last_month').hide();
+                    $('#net_profit_last_year').hide();
+                    $('#net_profit_this_year').show();
 
                     var ctx = document.getElementById("canvas").getContext('2d');
 
@@ -225,10 +228,9 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
                     var expense = result.expense;
                     var profit = result.profit;
 
-                    var labels = result.date; // Add labels to array
-                    // End Defining data
+                    var labels = result.month; // Add labels to array
 
-                    // End Defining data
+
                     if (window.myCharts != undefined)
                         window.myCharts.destroy();
                     window.myCharts = new Chart(ctx, {
@@ -282,20 +284,12 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
             $.when(this._super())
                 .then(function (ev) {
 
-
-                    $('#toggle-two').bootstrapToggle({
-                        on: 'View All Entries',
-                        off: 'View Posted Entries'
-                    });
-
-
-                    var posted = false;
-                    if ($('#toggle-two')[0].checked == true) {
-                        posted = "posted"
-                    }
+                    var posted = "posted";
                     var today = new Date ();
                     var month_value = today.getFullYear ()+'-'+  ('0' + (today.getMonth()+1)).slice(-2);
-                    
+                    var sel = "income_this_year"
+                    document.getElementById("month_id").value = month_value;
+
                     rpc.query({
                         model: "account.move",
                         method: "get_currency",
@@ -305,11 +299,10 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
 
                         })
 
-
                     rpc.query({
                         model: "account.move",
                         method: "get_income_expense",
-                        args: ["income_this_year"],
+                        args: [sel,month_value],
                     })
                         .then(function (result) {
 
@@ -494,7 +487,8 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
 
                     rpc.query({
                         model: "account.move",
-                        method: "get_salary_list"
+                        method: "get_salary_list",
+                        args: [month_value],
                     }).then(function (result) {
                             var due_count = 0;
                             var amount;
@@ -514,7 +508,8 @@ odoo.define('AccountingDashboard.AccountingDashboard', function (require) {
 
                     rpc.query({
                         model: "account.move",
-                        method: "get_accounts_profit"
+                        method: "get_accounts_profit",
+                        args: [month_value],
                     })
                         .then(function (result) {
                             var net_profit_this_year = result[0].profit;
